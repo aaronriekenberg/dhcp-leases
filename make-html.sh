dc97b9683a63654d34656883f50279853829a3a1
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-01-05 14:34:08 -0600

    Use recode.

--- make-html.sh
+++ make-html.sh
@@ -5,5 +5,5 @@ OUTPUT_FILE=/var/www/htdocs/dhcp/leases.html
 cd /home/aaron/dhcp-leases
 
 cat top.html > $OUTPUT_FILE
-./show-leases.sh >> $OUTPUT_FILE 2>&1
+./show-leases.sh 2>&1 | recode 'utf8..html' >> $OUTPUT_FILE
 cat bottom.html >> $OUTPUT_FILE
