fb4796750d791173541583c7b847a01bdf98aa11
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-04-12 17:26:01 -0500

    Cleanup.

--- make-html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUTPUT_FILE=/var/www/htdocs/dhcp/leases.html
-
-cd /home/aaron/dhcp-leases
-
-cat top.html > $OUTPUT_FILE
-./show-leases.sh 2>&1 | recode 'utf8..html' >> $OUTPUT_FILE
-cat bottom.html >> $OUTPUT_FILE
